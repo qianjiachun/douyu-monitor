81e923263156917de069a92b6b5d2e2d31480144
qianjiachun <189964430@qq.com>
2021-11-10 20:27:19 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "@/global/styles/index.scss"
 
 createApp(App).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import importToCDN from 'vite-plugin-cdn-import'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,5 +16,10 @@ export default defineConfig({
         }
       ]
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+        '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
