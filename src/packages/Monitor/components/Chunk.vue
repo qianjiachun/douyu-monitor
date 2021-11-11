3c8f0403eef39023de38d8eff8dfc0f7265445aa
qianjiachun <189964430@qq.com>
2021-11-11 17:30:29 +0800

    init

---
+++ src/packages/Monitor/components/Chunk.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="chunk"></div>
+</template>
+
+<script setup>
+import {ref, computed} from 'vue'
+let props = defineProps({
+    order: Number,
+    size: Number
+})
+
+let sizeStyle = computed(() => {
+    return `${String(props.size)}%`
+})
+let orderStyle = computed(() => {
+    return props.order
+})
+
+</script>
+
+<style scoped>
+.chunk {
+    width: 100%;
+    flex: 0 0 v-bind(sizeStyle);
+    order: v-bind(orderStyle);
+
+    background-color: pink;
+    border-radius: 10px;
+}
+</style>
--- src/packages/Monitor/pages/index.vue
+++ src/packages/Monitor/pages/index.vue
@@ -1,17 +1,43 @@
 <template>
-    <div class="haha">
-
+    <div class="monitor">
+        <Chunk v-for="(item, index) in chunks" :key="item.id"
+            :order="item.order"
+            :size="item.size"
+        ></Chunk>
     </div>
 </template>
 
 <script setup>
 import {ref} from 'vue'
+import Chunk from "../components/Chunk.vue"
+let chunks = ref([
+    {
+        id: 0,
+        order: 0,
+        size: 30,
+    },
+    {
+        id: 1,
+        order: 1,
+        size: 30,
+    },
+    {
+        id: 2,
+        order: 2,
+        size: 10,
+    },
+])
 
 </script>
 
 <style lang="scss" scoped>
-.haha {
-    display: block;
-    
+.monitor {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    padding: 10px;
+    box-sizing: border-box;
 }
 </style>
