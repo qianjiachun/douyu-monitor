a4cc5146e3108975d3b37ced6d5d94f0525ce7e5
qianjiachun <189964430@qq.com>
2022-10-18 15:58:15 +0800

    feat: add memlab to analyse memory

--- remix/memlab/index.js
+++ remix/memlab/index.js
@@ -1,12 +1,10 @@
-const {takeSnapshots, StringAnalysis, analyze, findLeaks} = require('@memlab/api');
+const {takeSnapshots, findLeaks} = require('@memlab/api');
 
 (async function () {
   const scenario = {
     url: () => "http://localhost:3000/1126960",
   };
   const result = await takeSnapshots({scenario});
-  const analysis = new StringAnalysis();
   const leaks = findLeaks(result);
   console.log(leaks);
-  // await analyze(result, analysis);
 })();
