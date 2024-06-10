404598d37dfe43e8d58fefd4c42ccb071c4c89e8
czwang <1244698585@qq.com>
2024-06-10 13:13:41 +0800

    feat: 鼠标移动分割线可调整模块占比
    feat: 鼠标移动分割线可调整模块占比
    
    bug: #10 处理通过布局调整模块顺序后控制占比失效问题
    
    bug: #10 分割线阻止默认事件弹出设置面板

--- remix/app/components/SplitLine/index.tsx
+++ remix/app/components/SplitLine/index.tsx
@@ -1,25 +1,91 @@
-import { memo } from "react";
-import type { FC } from "react";
+import React, { useState, useCallback, useContext, memo } from "react";
+import { OPTIONS_ACTION, OptionsContext } from "~/hooks/options.reducer";
 
 interface IProps {
-    order: number;
-    transparent: boolean;
-    direction: "row" | "column";
+  order: number;
+  transparent: boolean;
+  direction: "row" | "column";
 }
 
-const SplitLine: FC<IProps> = (props) => {
+const SplitLine: React.FC<IProps> = ({ order, transparent, direction }) => {
+
+  const [isDragging, setIsDragging] = useState(false);
+  const { state, dispatch } = useContext(OptionsContext);
+
+  const onMouseDown = useCallback((event: any) => {
+    event.stopPropagation && event.stopPropagation();
+    event.preventDefault && event.preventDefault();
+    setIsDragging(true);
+    document.body.style.cursor = direction === "row" ? "col-resize" : "row-resize";
+
+    const boxSizes = JSON.parse(JSON.stringify(state.size));
+    const windowLength: number = direction === "row" ? document.documentElement.clientWidth : document.documentElement.clientHeight;
+    const boxName: IOptionsSwitch = order === 2 ? state.switch[0] : order === 4 ? state.switch[1] : order === 6 ? state.switch[2] : state.switch[3];
+    const boxIndex: number = state.switch.indexOf(boxName);
+
+    const onMouseMove = (event: MouseEvent) => {
+      let _boxLength: number = direction === "row" ? event.clientX : event.clientY;
+      if (boxIndex === 1) {
+        _boxLength -= boxSizes[state.switch[0]] / 100 * windowLength + 12;
+      }
+      if (boxIndex === 2) {
+        _boxLength -= boxSizes[state.switch[0]] / 100 * windowLength + boxSizes[state.switch[1]] / 100 * windowLength + 24;
+      }
+      setOption(boxName, Math.floor(_boxLength / windowLength * 100));
+    };
+
+    const onMouseUp = () => {
+      event.stopPropagation && event.stopPropagation();
+      event.preventDefault && event.preventDefault();
+      setIsDragging(false);
+      document.body.style.cursor = "";
+      document.removeEventListener("mousemove", onMouseMove);
+      document.removeEventListener("mouseup", onMouseUp);
+    };
+
+
+    const setOption = (name: string, size: number) => {
+      switch (name) {
+        case "enter": dispatch({ type: OPTIONS_ACTION.SIZE, payload: { enter: size } }); break;
+        case "gift": dispatch({ type: OPTIONS_ACTION.SIZE, payload: { gift: size } }); break;
+        case "danmaku": dispatch({ type: OPTIONS_ACTION.SIZE, payload: { danmaku: size } }); break;
+        default: dispatch({ type: OPTIONS_ACTION.SIZE, payload: { superchat: size } });
+      }
+    }
+
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+
+  }, [direction, state]);
+
+
+  const onClick = (event: any) => {
+    event.stopPropagation && event.stopPropagation();
+    event.preventDefault && event.preventDefault();
+  };
+
   return (
-    <div
-      className="splitline"
-      style={{
-        order: props.order,
-        width: props.direction === "row" ? "3px" : "100%",
-        height: props.direction === "row" ? "100%" : "3px",
-        ...props.transparent ? {backgroundColor: "transparent"} : {}
-      }}
-    ></div>
+    // 嵌套一层实现鼠标移入选取范围变大
+    <div style={{
+      order,
+      width: direction === "row" ? "12px" : "100%",
+      minWidth: direction === "row" ? "12px" : "100%",
+      height: direction === "row" ? "100%" : "12px",
+      minHeight: direction === "row" ? "100%" : "12px",
+      cursor: direction === "row" ? "col-resize" : "row-resize",
+      display: "flex"
+    }} onMouseDown={(e) => onMouseDown(e)} onClick={(e) => onClick(e)}>
+      <div
+        className="splitline"
+        style={{
+          order,
+          width: direction === "row" ? "3px" : "100%",
+          height: direction === "row" ? "100%" : "3px",
+          margin: direction === "row" ? "0 auto" : "auto 0",
+          ...transparent ? { backgroundColor: "transparent" } : {}
+        }}></div>
+    </div>
   );
 };
-
 
 export default memo(SplitLine);
